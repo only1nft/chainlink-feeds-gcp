9e9ba15184f0ea9b799916dba07c924714f39513
Dan <7160998+dn-l@users.noreply.github.com>
2022-06-20 07:47:39 +0900

    CORS

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ function getLatestRate(feedId) {
 }
 
 exports.price = async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  }
   const { id = "" } = req.query;
   const feedId = FEEDS[id.toUpperCase()];
   if (!feedId) {
